04a8a650beb016feeeb94c328524f9c19a82f31a
leria <khudobavaleria@gmail.com>
2026-04-10 14:48:17 +0300

    edit

--- tests/test_text_filter.py
+++ tests/test_text_filter.py
@@ -1,6 +1,10 @@
+import sys
+import os
+
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 
 import pytest
-import os
 import tempfile
 from text_filter import (
     read_file,
